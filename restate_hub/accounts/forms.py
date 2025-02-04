6cf95b34adbc8363b5770c109f6d196c24aaf30b
Ardhweb <sun.projectsdev@gmail.com>
2025-02-04 23:37:04 +0530

    Show Password added at Signup Page

--- restate_hub/accounts/forms.py
+++ restate_hub/accounts/forms.py
@@ -1,19 +1,20 @@
 from django import forms
 # from django.contrib.auth.models import User
 from accounts.models import User
+
 class LoginForm(forms.Form):
     username_or_email = forms.CharField(label='Username or Email',widget=forms.TextInput(attrs={'class':'form-control'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
     
 class SignupForm(forms.ModelForm):
     password = forms.CharField(label='Password',widget=forms.PasswordInput(attrs={'class':'form-control', "id":"nomd_deS8"}))
-    password2 = forms.CharField(label='Repeat Password', widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    password2 = forms.CharField(label='Repeat Password', widget=forms.PasswordInput(attrs={'class':'form-control',"id":"disci_R6"}))
     class Meta:
         model = User
         fields = ['username','email','member_type','message']
         widgets ={
                 'username':forms.TextInput(attrs={'class':'form-control w-100'}),
-                'email':forms.TextInput(attrs={'class':'form-control'}),
+                'email':forms.EmailInput(attrs={'class':'form-control'}),
                 'member_type':forms.HiddenInput(attrs={'class':'form-control','id':'member_type_vale'}),
                 'message':forms.Textarea(attrs={'class':'form-control','rows':'3'}),
             }
