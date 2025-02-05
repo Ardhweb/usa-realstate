1ef8f9ffc8fc784584ea8089a330c4ba5a52ae82
Ardhweb <sun.projectsdev@gmail.com>
2025-02-05 22:16:18 +0530

    Update models.py
    -Alter the Agent Col from Seller Table
    -Removed Buyers,  Investors, etc fields
    
    Co-Authored-By: Shubumbarkar <154700592+Shubumbarkar@users.noreply.github.com>

--- restate_hub/seller_module/models.py
+++ restate_hub/seller_module/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from core.models import BaseModel
 import shortuuid
-from buyer_module.models import Buyers
+from agent_module.models import Agents
 # Create your models here.
 Foreclosure = 'Foreclosure'
 Retirement = 'Retirement'
@@ -34,10 +34,7 @@ class Sellers(BaseModel):
     last_name =  models.CharField(max_length=50,blank=False, null=True)
     phone_num =  models.IntegerField()
     email = models.EmailField(max_length=254)
-    agent_id = models.BigIntegerField(blank=False, null=True) # needs to chnage into fk near future.
-    buyer = models.ForeignKey(Buyers, on_delete=models.SET_NULL,null=True)
-    investor_id = models.BigIntegerField(blank=False, null=True) # needs to chnage into fk near future.
-    property_id = models.BigIntegerField(blank=False, null=True) # needs to chnage into fk near future.
+    agent = models.ForeignKey(Agents, on_delete=models.SET_NULL, null=True, blank=True)
     agent_rep = models.CharField(max_length=50,blank=False, null=True)
     bac = models.BooleanField(default=False)
     bac_fee = models.DecimalField(max_digits=10, decimal_places=2)
