cda8bc92826c3fff13b794f2abae31cd0f5a2f34
Ardhweb <sun.projectsdev@gmail.com>
2025-02-15 22:50:10 +0530

    Seller Dashboard Layout Ready

--- restate_hub/seller_module/views.py
+++ restate_hub/seller_module/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render ,redirect
 # Create your views here.
 
 def seller_dashboard(request):
-    if request.user.is_authenticated and user.member_type == 'seller':
+    if request.user.is_authenticated:
         return render(request, "seller/dashboard.html")
     else:
         return redirect('home')
