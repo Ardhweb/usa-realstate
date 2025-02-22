7b6e013bfb64d1bfb2ca4d1a036b720cf596428b
Ardhweb <sun.projectsdev@gmail.com>
2025-02-22 14:26:34 +0530

    Update urls.py

--- restate_hub/dashboard/urls.py
+++ restate_hub/dashboard/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path 
 from . import views 
-from . import utils
+
 app_name="dashboard"
 
 urlpatterns = [
     path('', views.admin_custom_dashboard, name="dashboard"),
     path('membership-fee/', views.membership_fee, name="membershipfee"),
     path('accounts/',views.accounts_page, name="accounts_page"),
-    path('user_data/',  utils.user_data, name='user_data'),
+   
 ]
