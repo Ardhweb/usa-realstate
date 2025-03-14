820b5b8e35ef48a4834dd5b61da3ed32aea4829e
Ardhweb <sun.projectsdev@gmail.com>
2025-03-14 22:50:44 +0530

    AddingProperty+
    -in Addproperty page we added new Location Form for Geolocation and lat and long values because of searching feature that  asked recently for it,
    
    Co-Authored-By: Shubumbarkar <154700592+Shubumbarkar@users.noreply.github.com>

--- restate_hub/property_module/forms.py
+++ restate_hub/property_module/forms.py
@@ -1,15 +1,14 @@
 from django import forms
-from property_module.models import PropertiesInfo
+from property_module.models import PropertiesInfo,PropertyLocation
 class SearchPropertyForm(forms.Form):
    pass
 
 class AddPropertiesInfoForm(forms.ModelForm):
-
    class Meta:
       model = PropertiesInfo
       fields = '__all__'
       widgets ={
-               'country':forms.Select(attrs={'aria-label':"Default select example",'class':'form-select', 'id':'country-list','onchange':"get_States(this.value)"}),
+               'country':forms.HiddenInput(attrs={'aria-label':"Default select example",'class':'form-select', 'id':'country-list','onchange':"get_States(this.value)"}),
                'state':forms.Select(attrs={'aria-label':"Default select example",'class':'form-select form-select', 'id':'state-list','onchange':"get_Counties(this.value)"}),
                'county':forms.Select(attrs={'aria-label':"Default select example",'class':'form-select form-select', 'id':'county-list','onchange':"get_Cities(this.value)"}),
                'city':forms.Select(attrs={'aria-label':"Default select example",'class':'form-select form-control select2', 'id':'city-list'}),
@@ -23,7 +22,8 @@ class AddPropertiesInfoForm(forms.ModelForm):
    #       field.widget.attrs.update({'class': 'form-control'})
    def __init__(self, *args, **kwargs):
       super().__init__(*args, **kwargs)
-      required_fields = ['country','city', 'state','county']
+      required_fields = ['city', 'state','county']
+      self.fields['country'].initial = "United States"
       for field_name, field in self.fields.items():
          if field_name not in ['country','city', 'county','state','image']:  # Exclude city and state fields
             field.widget.attrs.update({'class': 'form-control'})
@@ -32,6 +32,17 @@ class AddPropertiesInfoForm(forms.ModelForm):
             field.required = True
 
 
+class PropertyLocationForm(forms.ModelForm):
+
+   class Meta:
+      model = PropertyLocation
+      fields = ['latitude', 'longitude']
+      widgets ={
+               'latitude':forms.NumberInput(attrs={"step": "any", 'class':'form-control', }),
+               'longitude':forms.NumberInput(attrs={"step": "any", 'class':'form-control', }),
+              
+               
+            }
 
 class ContactPartiesForm(forms.Form):
    property_id = forms.CharField(required=False,widget=forms.HiddenInput(attrs={'class':'form-control',}))
--- restate_hub/property_module/views.py
+++ restate_hub/property_module/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from core.models import City,State,Country
-from property_module.models import PropertiesInfo
+from property_module.models import PropertiesInfo , PropertyLocation
 from django.http import JsonResponse,Http404
 import json
 from django.core.exceptions import ObjectDoesNotExist
-from property_module.forms import AddPropertiesInfoForm, ContactPartiesForm
+from property_module.forms import AddPropertiesInfoForm, ContactPartiesForm , PropertyLocationForm
 from django.contrib.auth.decorators import login_required
 from message_track.models import MessageTrack
 from django.core.mail import send_mail
@@ -23,17 +23,26 @@ def add_property(request):
     if request.user.is_authenticated and request.user.member_type == 'seller' or request.user.member_type == 'agent':
         if request.method == 'POST':
             form = AddPropertiesInfoForm(request.POST,request.FILES)
+            location_form = PropertyLocationForm(request.POST)
             seller =  Sellers.objects.get(user=request.user)
-            if form.is_valid():
+            if form.is_valid() and location_form.is_valid():
                 instance = form.save(commit=False)
                 instance.seller = seller
                 instance.save()
+                
+                location_instance = location_form.save(commit=False)
+                location_instance.property = instance
+                if location_form.cleaned_data['latitude'] == None and location_form.cleaned_data['longitude'] is None:
+                    return redirect('property_module:property-mine')  # Use return instead of break
+                
+                location_instance.save()
                 return redirect('property_module:property-mine')
             else:
-                print(form.errors)  # Debugging: Print form validation errors
+                print(form.errors, location_form.errors)  # Debugging: Print form validation errors
         else:
             form = AddPropertiesInfoForm()
-        return render(request, 'property/add_property.html', {'form': form})
+            location_form = PropertyLocationForm()
+        return render(request, 'property/add_property.html', {'form': form,'location_form':location_form})
     else:
         raise Http404('Page not found')
 
